133cff7b1bc66239184d152218a17933f0ba5ed8
priyanshu singh <priyanshusing8000@gmail.com>
2025-07-27 20:59:14 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FeatureSection from "./components/FeatureSection"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Workflow from "./components/Workflow"
+import Price from './components/Price'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <FeatureSection />
       <Workflow />
+      <Price />
       </div>
     </>
   )
